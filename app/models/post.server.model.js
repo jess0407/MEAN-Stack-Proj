121a58b9b9af7896abf238551256614056fbf855
Jessie Shi <jessie.shi@me.com>
2015-02-19 12:45:13 +0000

    dnews init & post module

---
+++ app/models/post.server.model.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Post Schema
+ */
+var PostSchema = new Schema({
+	title: {
+		type: String,
+		default: '',
+		required: 'Please fill Post name',
+		trim: true
+	},
+	url: {
+		type: String,
+		default: '',
+		required: 'Please fill url',
+		trim: true
+	},
+	upvotes: {
+		type: Number,
+		default: 0
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Post', PostSchema);
